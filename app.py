95c8e34e338f9df04d6ccb5c026d4c4e7808a944
arnaud_dg <ad_learning@yahoo.com>
2023-11-29 00:04:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 # Importation des packages
 from pathlib import Path
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
-# ... autres importations ...
+from ultralytics import YOLO
+from streamlit_webrtc import VideoProcessorBase, RTCConfiguration, WebRtcMode, webrtc_streamer, VideoTransformerBase
+import numpy as np
+from PIL import Image
+import av
+import cv2
+import settings
 
 # Paramètres et fonctions de settings.py et helper.py
 # ... contenu de settings.py ...
@@ -10,21 +15,63 @@ from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
 
 # Configuration de la page Streamlit
 st.set_page_config(
-    page_title="Détection d'objets avec Webcam",
+    page_title="Ergonomy Detection Bot",
     page_icon="🤖",
     layout="wide",
     initial_sidebar_state="expanded"
 )
 
 # Titre et description
-st.title("Détection d'objets avec Webcam")
-st.markdown('Ouvrez votre webcam et cliquez sur le bouton "Détecter les objets" pour commencer.')
+st.title("Ergonomy Detection Bot")
+st.markdown('Ouvrez votre webcam et cliquez sur le bouton "Start" pour commencer l'acquisition.')
+
+class MyVideoTransformer(VideoTransformerBase):
+    def __init__(self, conf, model):
+        self.conf = conf
+        self.model = model
+
+    def recv(self, frame):
+        image = frame.to_ndarray(format="bgr24")
+        processed_image = self._display_detected_frames(image)
+        st.image(processed_image, caption='Detected Video', channels="BGR", use_column_width=True)
+
+    def _display_detected_frames(self, image):
+        orig_h, orig_w = image.shape[0:2]
+        width = 720  # Set the desired width for processing
+
+        # cv2.resize used in a forked thread may cause memory leaks
+        input = np.asarray(Image.fromarray(image).resize((width, int(width * orig_h / orig_w))))
+
+        if self.model is not None:
+            # Perform object detection using YOLO model
+            res = self.model.predict(input, conf=self.conf)
+
+            # Plot the detected objects on the video frame
+            res_plotted = res[0].plot()
+            return res_plotted
+
+        return input
+
 
 # Configuration de la webcam
 def play_webcam(conf, model):
-    # ... code de la fonction play_webcam de helper.py ...
+    """
+    Plays a webcam stream. Detects Objects in real-time using the YOLO object detection model.
+
+    Returns:
+        None
+
+    Raises:
+        None
+    """
+    webrtc_streamer(
+        key="example",
+        video_transformer_factory=lambda: MyVideoTransformer(conf, model),
+        # rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
+        media_stream_constraints={"video": True, "audio": False},
+    )
 
 # Lancement de la webcam
 confidence = 0.4  # Définir la valeur de confiance
-model = load_model(Path('chemin/vers/le/modèle'))  # Charger le modèle
+model = YOLO('yolov8n-pose.pt')  # Charger le modèle
 play_webcam(confidence, model)
