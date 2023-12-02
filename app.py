fceb34ccb0497ea80d4219baa1b53b55c0d74270
arnaud_dg <ad_learning@yahoo.com>
2023-12-02 15:23:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ def play_webcam(conf, model):
     webrtc_streamer(
         key="example",
         # video_processor_factory=VideoProcessor,
-        video_transformer_factory=lambda: MyVideoTransformer(conf, model),
+        video_processor_factory=lambda: MyVideoTransformer(conf, model),
         rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
         media_stream_constraints={"video": True, "audio": False},
     )
