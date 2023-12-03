9f23167600c734b4d8b76bbe5004e17e57c06bdd
arnaud_dg <ad_learning@yahoo.com>
2023-12-03 10:26:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -33,8 +33,8 @@ class MyVideoTransformer(VideoTransformerBase):
         result_keypoint = results[0].keypoints.xyn.cpu().numpy()[0]
         print(result_keypoint)
         # Mettre à jour le dataframe avec les nouveaux résultats
-        new_data = {'Resultat': result_keypoint}
-        df = df.append(new_data, ignore_index=True)
+        # new_data = {'Resultat': result_keypoint}
+        # df = df.append(new_data, ignore_index=True)
         return results[0].plot()
 
 tab1, tab2 = st.tabs(["Acquisition", "Report"])
