2885fdfd74ff3739f2741085ebc9e691e2a8edf2
arnaud_dg <ad_learning@yahoo.com>
2023-12-03 10:02:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ import numpy as np
 # Basic configuration and title for the Streamlit app
 st.set_page_config(page_title="Ergonomy Detection Bot", page_icon="🤖", layout="wide")
 st.title("Ergonomy Detection Bot")
-st.markdown('Ouvrez votre webcam et cliquez sur le bouton Start pour commencer l\'acquisition.')
 
 class MyVideoTransformer(VideoTransformerBase):
     def __init__(self):
@@ -26,8 +25,25 @@ class MyVideoTransformer(VideoTransformerBase):
         print(result_keypoint)
         return results[0].plot()
 
-# Stream webcam with YOLO model
-webrtc_streamer(key="example", 
-                video_processor_factory=MyVideoTransformer, 
-                rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
-                media_stream_constraints={"video": True, "audio": False})
+tab1, tab2 = st.tabs(["Acquisition", "Report"])
+
+with tab1:
+    st.markdown('Ouvrez votre webcam et cliquez sur le bouton Start pour commencer l\'acquisition.')
+
+    col1, col2 = st.columns([3, 1])
+    
+    # Stream webcam with YOLO model
+    col1.subheader("Webcam caster")
+    col1.webrtc_streamer(key="example", video_processor_factory=MyVideoTransformer, rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}, media_stream_constraints={"video": True, "audio": False})
+
+    # Stream webcam with YOLO model
+    col2.subheader("Alert")
+    if len(results) == 0:
+        col2.text('No one is on the film')
+    elif len(results) >= 2:
+        col2.text('More than 1 person on the movie')
+    else:
+        col2.text('1 person')
+
+with tab2:
+    st.markdown('Sorry, you haven't acquired anything ! ')
