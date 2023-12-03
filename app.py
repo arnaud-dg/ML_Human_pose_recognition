2d70a02912fab2b9121b95f3d14d417e2685794d
arnaud_dg <ad_learning@yahoo.com>
2023-12-03 10:09:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ import numpy as np
 st.set_page_config(page_title="Ergonomy Detection Bot", page_icon="🤖", layout="wide")
 st.title("Ergonomy Detection Bot")
 
+results = []
+
 class MyVideoTransformer(VideoTransformerBase):
     def __init__(self):
         self.model = YOLO('yolov8n-pose.pt')
@@ -29,21 +31,8 @@ tab1, tab2 = st.tabs(["Acquisition", "Report"])
 
 with tab1:
     st.markdown('Ouvrez votre webcam et cliquez sur le bouton Start pour commencer l\'acquisition.')
-
-    col1, col2 = st.columns([3, 1])
-    
     # Stream webcam with YOLO model
-    col1.subheader("Webcam caster")
     webrtc_streamer(key="example", video_processor_factory=MyVideoTransformer, rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}, media_stream_constraints={"video": True, "audio": False})
-
-    # Stream webcam with YOLO model
-    col2.subheader("Alert")
-    if len(results) == 0:
-        col2.text('No one is on the film')
-    elif len(results) >= 2:
-        col2.text('More than 1 person on the movie')
-    else:
-        col2.text('1 person')
 
 with tab2:
     st.markdown('Sorry, you haven t acquired anything ! ')
